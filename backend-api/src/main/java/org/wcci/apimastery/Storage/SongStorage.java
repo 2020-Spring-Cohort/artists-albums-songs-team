5dd837889a2baf2b191b3e6650c28df121e06cf7
Colin Wakefield <59844720+colinwake@users.noreply.github.com>
2020-03-16 14:07:27 -0400

    Song Storage
    Defined methods for our JPA implementation to override
    
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

---
+++ backend-api/src/main/java/org/wcci/apimastery/Storage/SongStorage.java
@@ -0,0 +1,25 @@
+package org.wcci.apimastery.Storage;
+
+import org.wcci.apimastery.models.Album;
+import org.wcci.apimastery.models.Artist;
+import org.wcci.apimastery.models.Song;
+
+import java.util.Optional;
+
+public interface SongStorage {
+    
+    Optional<Song> findSongByTitle(String title);
+    
+    Iterable<Song> findAllByArtist(Artist artist);
+    
+    Iterable<Song> findAllByArtistName(String name);
+    
+    Iterable<Song> findAllByArtistLabel(String label);
+    
+    Iterable<Song> findAllByAlbum(Album album);
+    
+    Iterable<Song> findAllyByAlbumTitle(String title);
+    
+    Iterable<Song> findByBlackShirts(String blackShirts);
+    
+}
