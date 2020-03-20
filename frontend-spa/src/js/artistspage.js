4c30c0d2c9ad4dbff8edab69aef511c6f101882c
ben_beers <beerscb@gmail.com>
2020-03-20 14:48:12 -0400

    add collective artist functionality

---
+++ frontend-spa/src/js/artistspage.js
@@ -0,0 +1,27 @@
+import {
+    DomMaker
+} from './DomMaker.js'
+import {
+    createNewArtistForm
+} from './NewArtistForm.js'
+
+const artistPic = document.querySelector('.artistPic');
+
+const displayArtists = (artists) => {
+    artistPic.innerHTML='';
+    let artistList = new DomMaker('ul')
+        .changeContent('List of artists:');
+     artists.forEach((artist) => {
+        artistList.appendChild(new DomMaker('li')
+            .changeContent(artist.getImageLink())
+            .render())
+    });
+
+    anchorElement.append(artistList.render());
+    anchorElement.append(createNewArtistForm());
+}
+
+
+export {
+    displayArtists
+}
---
+++ frontend-spa/src/js/newartistform.js
@@ -0,0 +1,55 @@
+import{
+    DomMaker
+    } from './DomMaker.js'
+    import{
+        InputMaker
+    } from './InputMaker.js'
+    import{getAllArtists} from './artistbuilder.js'
+    
+    const createNewArtistForm= ()=>{
+        const form = new DomMaker('form')
+                        .changeContent('Add new Artist:')
+                        .appendChild(new DomMaker('Name')
+                                        .changeContent('Name:')
+                                        .render())
+                        .appendChild(new DomMaker('Age')
+                                        .changeContent('Age:')
+                                        .render())
+                        .appendChild(new DomMaker('Label')
+                                        .addClassName('form__description')
+                                        .render())
+                        .appendChild(new DomMaker('Albums')
+                                        .addClassName('form__description')
+                                        .render())
+                        .appendChild(new DomMaker('Songs')
+                                        .addClassName('form__description')
+                                        .render())
+                        .appendChild(new DomMaker('button')
+                                        .changeContent('Submit New Artist')
+                                        .addOnClickAction(()=>{
+                                         collectData();
+                                    })
+
+                                    .render())
+        return form;
+    }
+    
+    const collectData = () =>{
+        const newName = document.querySelector('.form__name');
+        const newDescription = document.querySelector('.form__description');
+    
+        const newArtistJson = {
+            "name": newArtist.value,
+            "description": newArtist.value
+        }
+
+        fetch("http://localhost:8080/artists/", {
+            method: 'POST', 
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newArtistJson) 
+          }).then(()=> getAllArtists());
+    }
+    
+    export {createNewArtistForm}
